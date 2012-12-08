c00595b8dc6e2968679cb1958fa2828eb1dc5160
jkroso <jkroso@gmail.com>
2012-12-09 01:05:55 +1300

    Switched to using enumerable

--- src/resolvers/hash.js
+++ src/resolvers/hash.js
@@ -1,40 +1,36 @@
-var find = require('lodash').find
+var Enumerable = require('enumerable-component')
 var core = require('path').resolve(__dirname, '../node_modules') + '/'
 
 module.exports = [
     function node_modules (hash, base, name) {
         if (hash[core+name+'.js']) return hash[core+name+'.js']
-        return find(
-            [
-                name,
-                name+'.js',
-                name+'.json',
-                name+'/index.js',
-                name+'/index.json',
-                name+'/package.json',
-            ]
-            .map(function (p) {
-                return base+'/node_modules/'+p
-            }),
-            function (p) {
-                return !!hash[p]
-            }
-        )
+        return Enumerable([
+            name,
+            name+'.js',
+            name+'.json',
+            name+'/index.js',
+            name+'/index.json',
+            name+'/package.json',
+        ])
+        .map(function (p) {
+            return base+'/node_modules/'+p
+        })
+        .find(function (p) {
+            return !!hash[p]
+        })
     },
     function components (hash, base, name) {
-        return find(
-            [
-                // Check for an alias...
-                name,
-                // ...and a real component
-                name+'/component.json'
-            ]
-            .map(function (p) {
-                return base+'/components/'+p
-            }),
-            function (p) {
-                return !!hash[p]
-            }
-        )
+        return Enumerable([
+            // Check for an alias...
+            name,
+            // ...and a real component
+            name+'/component.json'
+        ])
+        .map(function (p) {
+            return base+'/components/'+p
+        })
+        .find(function (p) {
+            return !!hash[p]
+        })
     }
 ]
