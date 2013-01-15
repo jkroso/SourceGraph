15174e87c9d3a7e1589e1548b172ce322daf41bc
jkroso <jkroso@gmail.com>
2013-01-15 23:41:26 +1300

    update example to use plugins

--- example/index.js
+++ example/index.js
@@ -1,46 +1,17 @@
-var detective = require('detective')
-  , path = require('path')
-  , Graph = require('../src')
-  , resolve = require('path').resolve
+var Graph = require('../src')
   , prettyjson = require('prettyjson').render
 
-var entry = resolve(__dirname, 'husband.js');
+/*!
+ * The main file of the program
+ */
+var entry = __dirname + '/husband.js';
 
 new Graph()
-	.addType({
-		if: /\.js$/,
-		make: Module
-	})
-	.trace(entry).then(function (files) {
+  .use('javascript')
+	.trace(entry)
+  .then(function (files) {
 		files.forEach(function (file) {
 			file.requires = file.requires()
 		})
 		console.log(prettyjson(files))
 	})
-
-
-
-module.exports = Module
-
-/**
- * Represents a module
- * @param {Object} a file object
- */
-function Module (file) {
-    var location = file.path
-    this.path = location
-    this.base = path.dirname(location)
-    this.ext = path.extname(location)
-    this.name = path.basename(location, this.ext)
-    this.ext = this.ext.replace(/\./, '')
-    this.text = file.text
-    this.lastModified = file['last-modified']
-}
-
-/**
- * List all files mention in require() statements
- * @return {Array} absolute paths
- */
-Module.prototype.requires = function () {
-    return detective(this.text)
-}
