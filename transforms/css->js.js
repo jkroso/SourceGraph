828905ed6bbd3dfb73dc5ff2325e89625b074ec8
Jake Rosoman <jkroso@gmail.com>
2014-03-02 20:59:59 +1300

    escape closing tag
    not sure why this is necessary but it is

--- transforms/css->js.js
+++ transforms/css->js.js
@@ -1,7 +1,7 @@
 
 module.exports = function(css){
-  var tag = JSON.stringify('<script>' + css + '<\/script>')
+  var tag = JSON.stringify('<script>' + css + '<\\/script>')
   return ''
     + 'var domify = require("' + require.resolve('domify') + '")\n'
-    + 'document.head.appendChild(domify(' + tag + '))'
+    + 'document.head.appendChild(domify(' + tag + '))\n'
 }
