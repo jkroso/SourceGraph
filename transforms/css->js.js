5ae95fd63e384e1397a460f3914d1814fc96f105
Jake Rosoman <jkroso@gmail.com>
2014-03-02 21:18:24 +1300

    Release 0.9.7

--- transforms/css->js.js
+++ transforms/css->js.js
@@ -1,6 +1,6 @@
 
 module.exports = function(css){
-  var tag = JSON.stringify('<script>' + css + '<\\/script>')
+  var tag = JSON.stringify('<style>' + css + '</style>')
   return ''
     + 'var domify = require("' + require.resolve('domify') + '")\n'
     + 'document.head.appendChild(domify(' + tag + '))\n'
