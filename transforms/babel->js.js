8b466ba09fc464f042c6b702829297802176e995
Jake Rosoman <jkroso@gmail.com>
2015-08-09 16:24:06 +1200

    add babel-runtime plugin

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -1,7 +1,12 @@
 var babel = require('babel-core')
+var path = require('path')
+var plugin = path.dirname(require.resolve('babel-plugin-runtime/package'))
 
 module.exports = function(es6, path, options) {
   options = Object.create(options)
   options.filename = path
+  options.plugins = options.plugins
+    ? options.plugins.concat(plugin)
+    : [plugin]
   return babel.transform(es6, options).code
 }
