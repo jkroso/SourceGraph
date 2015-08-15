3cd6c38658b31b6602a83cab6b729b75e6decb93
Jake Rosoman <jkroso@gmail.com>
2015-08-15 16:42:51 +1200

    update babel-plugin-runtime name

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -1,6 +1,6 @@
 var babel = require('babel-core')
 var path = require('path')
-var plugin = path.dirname(require.resolve('babel-plugin-runtime/package'))
+var plugin = path.dirname(require.resolve('babel-plugin-jkroso-runtime/package'))
 
 module.exports = function(es6, path, options) {
   options = Object.create(options)
