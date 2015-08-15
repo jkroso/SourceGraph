dff1a7f8e100ee9ddc72c18f77a84a4d1855c3e8
Jake Rosoman <jkroso@gmail.com>
2015-08-15 14:10:16 +1200

    support babel-runtime-plugin again

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -5,12 +5,8 @@ var plugin = path.dirname(require.resolve('babel-plugin-runtime/package'))
 module.exports = function(es6, path, options) {
   options = Object.create(options)
   options.filename = path
-  // currently only works when installed with npm and not
-  // symlinked so needs to be off by default
-  if (options.abspaths) {
-    options.plugins = options.plugins
-      ? options.plugins.concat(plugin)
-      : [plugin]
-  }
+  options.plugins = options.plugins
+    ? options.plugins.concat(plugin)
+    : [plugin]
   return babel.transform(es6, options).code
 }
