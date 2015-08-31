3fddb22e1a7b3ed34d4870011f5c11659411b896
Jake Rosoman <jkroso@gmail.com>
2015-08-31 22:35:38 +1200

    default jsxPragma to 'JSX'

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -5,6 +5,7 @@ var plugin = path.dirname(require.resolve('babel-plugin-jkroso-runtime/package')
 module.exports = function(es6, path, options) {
   options = Object.create(options)
   options.filename = path
+  options.jsxPragma = options.jsxPragma || 'JSX'
   options.plugins = options.plugins
     ? options.plugins.concat(plugin)
     : [plugin]
