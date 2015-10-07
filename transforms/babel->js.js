2766579e5dbbcde0950a28a799ede0ab1ee19291
Jake Rosoman <jkroso@gmail.com>
2015-10-08 12:55:03 +1300

    pass plugin directly

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -1,12 +1,11 @@
+var runtime = require('babel-plugin-jkroso-runtime')
 var babel = require('babel-core')
 var JSX = require('jsx-to-js')
-var path = require('path')
-var plugin = path.dirname(require.resolve('babel-plugin-jkroso-runtime/package'))
 
 module.exports = function(es6, path, options) {
   options = Object.create(options || null)
   options.filename = path
   options.blacklist = (options.blacklist || []).concat('react')
-  options.plugins = (options.plugins || []).concat(plugin, JSX.babel_plugin)
+  options.plugins = (options.plugins || []).concat(JSX.babel_plugin, runtime)
   return babel.transform(es6, options).code
 }
