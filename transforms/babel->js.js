40395fac6673a165fabe9b9c768abba5ccf033bf
Jake Rosoman <jkroso@gmail.com>
2015-11-06 16:12:37 +1300

    handle stages

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -1,6 +1,11 @@
 var babel = require('babel-core')
-
-var presets = [require('babel-preset-es2015')]
+var stages = [
+  require('babel-preset-stage-0'),
+  require('babel-preset-stage-1'),
+  require('babel-preset-stage-2'),
+  require('babel-preset-stage-3'),
+  require('babel-preset-es2015'),
+]
 var plugins = [
   require('babel-plugin-syntax-jsx'),
   require('jsx-to-js').babel_plugin,
@@ -8,8 +13,9 @@ var plugins = [
 
 module.exports = function(es6, path, options) {
   options = Object.create(options || null)
+  var stage = typeof options.stage == 'number' ? options.stage : 4
   options.filename = path
-  options.presets = (options.presets || []).concat(presets)
+  options.presets = (options.presets || []).concat(stages[stage])
   options.plugins = (options.plugins || []).concat(plugins)
   return babel.transform(es6, options).code
 }
