aec9daba65b5809a62784f9a8ffcef9837b2efbb
Jake Rosoman <jkroso@gmail.com>
2015-11-06 16:34:50 +1300

    fix presets

--- transforms/babel->js.js
+++ transforms/babel->js.js
@@ -1,10 +1,11 @@
 var babel = require('babel-core')
+
+var es2015 = require('babel-preset-es2015')
 var stages = [
   require('babel-preset-stage-0'),
   require('babel-preset-stage-1'),
   require('babel-preset-stage-2'),
   require('babel-preset-stage-3'),
-  require('babel-preset-es2015'),
 ]
 var plugins = [
   require('babel-plugin-syntax-jsx'),
@@ -13,9 +14,9 @@ var plugins = [
 
 module.exports = function(es6, path, options) {
   options = Object.create(options || null)
-  var stage = typeof options.stage == 'number' ? options.stage : 4
+  var stage = typeof options.stage == 'number' ? options.stage : 2
   options.filename = path
-  options.presets = (options.presets || []).concat(stages[stage])
+  options.presets = (options.presets || []).concat(es2015, stages[stage])
   options.plugins = (options.plugins || []).concat(plugins)
   return babel.transform(es6, options).code
 }
